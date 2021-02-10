2114b39e77df89257573663885b967db75baf2d6
maxitosh <maxitoshs@gmail.com>
2021-02-10 11:58:59 +0300

    Shell fix

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata user_init.json
-python manage.py runserver 127.0.0.1:8000
+python manage.py runserver 0.0.0.0:8000
